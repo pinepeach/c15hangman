be5f043d5b1a6857658ffa1159d861e41bf8643f
pinepeach <55676817+pinepeach@users.noreply.github.com>
2019-11-27 13:35:35 -0800

    Add files via upload

---
+++ js/hangman.js
@@ -0,0 +1,134 @@
+let word_bank = {
+    'COMMITTEE': 'A group that is in charge of a specific task.',
+    'POTATO': 'Vegetable', 'contemplating': 'thinking for a long time',
+    'INDECISIVE': 'Not being able to settle an issue',
+    'SLEEPY': 'BCIT students',
+    'ASLEEP': 'Jason quach (yellow hoodie) in class',
+    'DATABASE': 'We don\'t know what\'s happening',
+    'GIRAFFE': 'Long-necked animal',
+    'QUAGGA': 'Extinct animal',
+    'FELICIA': 'Our leader',
+    'JESUS': ' That guy with the long hair'
+}
+
+
+let hangman_pics = ['img/7.gif', 'img/6.gif', 'img/5.gif', 'img/4.gif', 'img/3.gif', 'img/2.gif', 'img/1.gif', 'img/0.gif']
+
+let lives = 7
+let CorrectClicks = 0
+function createButtons() {
+    for (i = 65; i < 91; i++) {
+        if (i == 79){
+            document.write('<br><br>')
+        }
+        let btn = document.createElement('button')
+        btn.innerHTML = String.fromCharCode(i);
+        btn.id = 'button_id'
+        document.body.appendChild(btn)
+        btn.onclick = function buttonOnClick() {
+            console.log(btn.innerHTML + ' clicked')
+            btn.id = 'buttonClicked'
+            for (i = 0; i < wordList.length; i++) {
+                if (wordList[i] == btn.innerHTML) {
+                    blankList[i] = btn.innerHTML
+                    wordListJoin = blankList.join('  ')
+                    document.getElementById('word').innerHTML = wordListJoin
+                    CorrectClicks+=1
+
+                }
+            }
+            if (! wordList.includes(btn.innerHTML)){
+               lostlife()
+            }
+            Solved()
+        }
+    }
+}
+
+
+function lostlife(){
+    lives -= 1;
+    livestext.innerHTML = 'You have ' + lives + ' lives left';
+    changeImage()
+    if (lives == 0){
+        for (i = 65; i < 91; i++) {
+            try{
+                document.getElementById('button_id').id = 'buttonClicked'
+            }
+            catch{
+                console.log(i + ' is already gone')
+            }
+        }
+        UsersName()
+    }
+}
+
+
+
+function displayWord() {
+    let i = Math.floor(Math.random() * 11)
+    let randomWord = Object.keys(word_bank)
+    let randomHint = Object.values(word_bank)
+    return [randomWord[i], randomHint[i]]
+}
+
+function UsersName(){
+    let name = prompt('What\'s your name?')
+    word.innerHTML = 'GAME OVER ' + name
+    
+}
+
+function Solved(){
+    if (CorrectClicks == randomWord.length){
+        for (i = 65; i < 91; i++) {
+            try{
+                document.getElementById('button_id').id = 'buttonClicked'
+            }
+            catch{
+                console.log(i + ' is already gone')
+            }
+        }
+    }
+}
+
+function hideWords(randomWord) {
+    let wordList = randomWord.split('')
+    let wordReplace = '__ '.repeat(parseInt(wordList.length))
+    blankList = wordReplace.split(' ')
+    blankList.pop()
+    wordListJoin = blankList.join('  ')
+    document.getElementById('word').innerHTML = wordListJoin
+    return [wordList, blankList]
+}
+
+
+function refresh(){
+    window.location.reload(true)
+}
+document.getElementById('restart').onclick = refresh
+
+
+
+function changeImage() {
+    document.getElementById('hangmanImg').src = hangman_pics[lives]
+}
+
+
+
+
+
+createButtons()
+displayWordFunction = displayWord()
+randomWord = displayWordFunction[0]
+console.log(randomWord)
+randomHint = displayWordFunction[1]
+
+hint.innerHTML = 'Hint: '+randomHint
+
+hideWordsFunction = hideWords(randomWord)
+wordList = hideWordsFunction[0]
+blankList = hideWordsFunction[1]
+livestext.innerHTML = 'You have ' + lives + ' lives left'
+changeImage()
+
+
